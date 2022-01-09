588028242020bbfe756ac7a48450f53177d0f826
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-09 03:29:21 +0000

    require sample query and use function from that to get query results.

---
+++ routes/user-router.js
@@ -0,0 +1,25 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into /users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const userQueries = require('../db/sample-users-query');
+
+module.exports = () => {
+  router.get("/", (req, res) => {
+    userQueries.getUsers()
+      .then(users => {
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
