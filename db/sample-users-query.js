4ee4197e0bbf2217b627f925a7f264607ba1c3dc
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-09 03:45:02 +0000

    remove console.logs

--- db/sample-users-query.js
+++ db/sample-users-query.js
@@ -2,10 +2,8 @@ const { response } = require('express')
 const db = require('../lib/db')
 
 const getUsers = () => {
-  console.log(db);
   return db.query('SELECT * FROM users;')
     .then((res) => {
-      console.log(res.rows);
       return res.rows;
     })
     .catch(err => err.message);
