014547ca67d5f2c37973106c3d2ec193a2981f4a
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-09 03:40:54 +0000

    remove white space

--- server.js
+++ server.js
@@ -8,21 +8,6 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-// PG database client/connection setup
-// const { Pool } = require("pg");
-// const dbParams = {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// }
-// const db = new Pool(dbParams);
-// db.connect(() => {
-//   console.log('connected to database');
-//   console.log(db.query);
-// });
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -49,7 +34,6 @@ const usersRoutes = require("./routes/user-router");
 const conversationsRoutes = require("./routes/conversations-router");
 
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
 app.use("/cars", carsRoutes());
 app.use("/users", usersRoutes());
 app.use("/conversations", conversationsRoutes());
