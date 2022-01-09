71f88a825d12707891ce5dd7adf6ccbaaaf6d308
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-09 03:32:33 +0000

    Remove databse pool/connection setup from server.js file.

--- server.js
+++ server.js
@@ -9,10 +9,19 @@ const app = express();
 const morgan = require("morgan");
 
 // PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
-const db = new Pool(dbParams);
-db.connect();
+// const { Pool } = require("pg");
+// const dbParams = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// }
+// const db = new Pool(dbParams);
+// db.connect(() => {
+//   console.log('connected to database');
+//   console.log(db.query);
+// });
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -35,13 +44,15 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const carsRoutes = require("./routes/cars-router");
+const usersRoutes = require("./routes/user-router");
+const conversationsRoutes = require("./routes/conversations-router");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/cars", carsRoutes());
+app.use("/users", usersRoutes());
+app.use("/conversations", conversationsRoutes());
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -49,6 +60,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
+  // res.redirect('/cars');
   res.render("index");
 });
 
