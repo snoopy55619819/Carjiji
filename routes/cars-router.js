4c4ab4e64e411426ed3bbefbc60bfb126bcb2820
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-09 03:26:44 +0000

    add sample query for testing.

--- routes/cars-router.js
+++ routes/cars-router.js
@@ -1,16 +1,55 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into /users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
 const express = require('express');
-const router = express.Router();
-const sampleUsersQuery = require('../db/sample-users-query');
+const router  = express.Router();
+// const carQueries = require('../db/queries/car-queries')
 
-//GET /cars/
-router.get('/', (req, res) => {
-  sampleUsersQuery.getUsers()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch(err => err.mesage);
-});
+module.exports = (db) => {
 
-modules.exports = {
-  router
+  // Route: "/cars/"
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const cars = data.rows;
+        res.json({ cars });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  // Route: "/cars/:id"
+  router.get("/:id", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const cars = data.rows;
+        res.json({ cars });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const cars = data.rows;
+        res.json({ cars });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
 };
