76e0fc707937c22037454414c3f011b30685b920
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-10 00:46:29 +0000

    add seeds for conversations, messages and favorites.

---
+++ db/seeds/04_messages.sql
@@ -0,0 +1,21 @@
+INSERT INTO messages (id, conversation_id, message_sender_id, message_body, created_time)
+ VALUES
+ (1, 1, 1, 'Hello, is the item available?', '2021-01-01'),
+ (2, 1, 2, 'Hey, yes it is!', '2021-01-01'),
+ (3, 1, 1, 'Can you do $40,000 for the car?', '2021-01-01'),
+ (4, 1, 2, 'For you, sure!', '2021-02-01'),
+ (5, 1, 1, 'We got a deal!', '2021-03-01'),
+
+ (6, 2, 1, 'potato', '2021-01-01'),
+ (7, 2, 3, 'potato???', '2021-01-01'),
+ (8, 2, 1, 'bob?', '2021-01-01'),
+ (9, 2, 3, 'superbob?', '2021-02-01'),
+ (10, 2, 1, 'ULTRABOB!!!', '2021-03-01'),
+
+ (11, 3, 1, 'Hello', '2021-01-01'),
+ (12, 3, 3, 'yes?', '2021-01-01'),
+
+ (13, 4, 1, 'Can you do $4,000 for the car?', '2021-01-01'),
+ (14, 4, 2, 'wut?', '2021-02-01'),
+
+ (15, 5, 3, 'is car still for sale?', '2021-03-01');
---
+++ db/seeds/05_users_favourite_cars.sql
@@ -0,0 +1,10 @@
+INSERT INTO users_favorite_cars (id, user_id, car_id)
+ VALUES
+ (1, 1, 1),
+ (2, 1, 2),
+ (3, 1, 3),
+ (4, 1, 4),
+ (5, 1, 5),
+ (6, 2, 1),
+ (7, 2, 2),
+ (8, 3, 1);
