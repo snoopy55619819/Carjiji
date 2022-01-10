15d8319209cb1d3a92f83344e2fff5f72ab4d67d
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-10 04:42:39 +0000

    Create /conversations get request and use 2 queries with async-await to render conversations and associated messages as .json.

--- routes/conversations-router.js
+++ routes/conversations-router.js
@@ -7,16 +7,50 @@
 
 const express = require('express');
 const router  = express.Router();
-const userQueries = require('../db/queries/sample-users-query');
+const userQueries = require('../db/queries/user-queries');
+const carQueries = require('../db/queries/car-queries');
+const conversationQueries = require('../db/queries/conversation-queries');
 
 module.exports = () => {
 
   // GET Route: "/conversations"
   //  Show all conversations
+  /* Output rendered to page as .json:
+    {
+      convWithMessages: [
+        {
+          Conversation_ID: 1,
+          Buyer: "Kira",
+          Seller: "Alice",
+          Car: "2019 Acura RDX",
+          Car_Image: "https://i.ebayimg.com/images/g/5ZgAAOSw9ARh17iw/s-l640.webp",
+          messages: [{message_1_obj}, {message_2_obj}...]
+        },
+        {Conversation_ID: 2, ...},
+        {Conversation_ID: 3, ...}
+      ]
+    }
+  */
   router.get("/", (req, res) => {
-    userQueries.getUsers()
-      .then(conversations => {
-        res.json({ conversations });
+    conversationQueries.getAllConversationsForUser(1)
+    // Returns an array of conversations which is passed onto the next .then()
+    .then(conversations => {
+      return conversations
+    })
+    // Since async code in within this .then(), used async-await to run this code syncronously.
+    //  Take in an object of conversations -> query for messages with conversation_id
+    //  -> add the messages into corresponding conversation
+    .then(async conversations => {
+        const convWithMessages = [];
+        for (conv of conversations) {
+          const convID = conv.Conversation_ID;
+          await conversationQueries.getMessagesForConversation(convID)
+          .then((messages) => {
+            conv.messages = messages;
+            convWithMessages.push(conv);
+          })
+        }
+        res.json({ convWithMessages });
       })
       .catch(err => {
         res
@@ -28,7 +62,7 @@ module.exports = () => {
   // POST Route: "/conversations/:id"
   //  Reply to a conversation
   router.post("/:id", (req, res) => {
-    userQueries.getUsers()
+    conversationQueries.getAllConversationsForUser(1)
       .then(conversations => {
         res.json({ conversations });
       })
