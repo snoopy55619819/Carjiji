f06585b237b478ba1881f5e1d6d49eb26d3cc345
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-10 02:59:24 +0000

    create queries using actual populated midterm database

--- db/queries/user-queries.js
+++ db/queries/user-queries.js
@@ -1,10 +1,8 @@
 const { response } = require('express')
-const db = require('../lib/db')
+const db = require('../../lib/db')
 
 const getAllUsers = () => {
-  /*
-  SELECT * FROM users
-  */
+
   return db.query('SELECT * FROM users;')
     .then((res) => {
       return res.rows;
@@ -13,38 +11,19 @@ const getAllUsers = () => {
 };
 
 const getUserById = (user_id) => {
-  /*
-  (`
-  SELECT * FROM cars
-  WHERE cars.id = $1
-  `, [car_id])
-  */
-  return db.query(`SELECT * FROM users`, [car_id])
+
+  return db.query(`
+    SELECT * FROM users
+    WHERE users.id = $1
+  `, [user_id])
     .then((res) => {
       return res.rows[0];
     })
     .catch(err => err.message);
 };
 
-const getCarsForUser = (user_id) => {
-  /*
-  (`
-  SELECT * FROM cars
-  WHERE owner_id = $1
-  `, [user_id])
-  */
-  return db.query(`
-    SELECT *
-    FROM users
-  `, [user_id])
-    .then((res) => {
-      return res.rows;
-    })
-    .catch(err => err.message);
-};
 
-modules.exports = {
+module.exports = {
   getAllUsers,
-  getUserById,
-  getCarsForUser
+  getUserById
 };
