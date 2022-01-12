9abac5bbbbb2b0a59dde508bad0788f54b19de20
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-12 02:21:42 +0000

    add edit car form to /cars/new page. Will move to seperage page later

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,30 @@
 // Client facing scripts here
+$(() => {
+  console.log('jquery ready');
+
+  const $targetDiv = document.getElementById("newDIV");
+  const $btn = document.getElementById("show-New");
+
+  const $targetDiv2 = document.getElementById("editDIV");
+  const $btn2 = document.getElementById("show-Edit");
+
+  $(function () {
+    $( $targetDiv ).hide();
+    $( $btn2 ).hide();
+  });
+
+  $( $btn ).click(function() {
+    $( $btn ).hide();
+    $( $btn2 ).show();
+    $( $targetDiv ).show( "slow" );
+    $( $targetDiv2 ).hide();
+  });
+
+  $( $btn2 ).click(function() {
+    $( $btn ).show();
+    $( $btn2 ).hide();
+    $( $targetDiv ).hide();
+    $( $targetDiv2 ).show( "slow" );
+  });
+
+});
