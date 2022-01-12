e876e37f2a7cf3d6e52426dd7cfe64b4e4ffc261
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-12 05:03:09 +0000

    create script for show/hide edit car section if user is owner

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,29 +2,63 @@
 $(() => {
   console.log('jquery ready');
 
-  const $targetDiv = document.getElementById("newDIV");
-  const $btn = document.getElementById("show-New");
+//card-with-details
+//card-with-edits
+//edit-car-toggle -> button
+//edit-car-toggle-cancel -> cancel button
 
-  const $targetDiv2 = document.getElementById("editDIV");
-  const $btn2 = document.getElementById("show-Edit");
+  const $divWithDetails = document.getElementById("card-with-details");
+  const $divWithEdits = document.getElementById("card-with-edits");
+
+  const $btnOpenEdit = document.getElementById("edit-car-toggle");
+  const $btnCloseEdit = document.getElementById("edit-car-toggle-cancel");
 
   $(function () {
-    $( $targetDiv ).hide();
-    $( $btn2 ).hide();
+    $( $divWithEdits ).hide();
   });
 
-  $( $btn ).click(function() {
-    $( $btn ).hide();
-    $( $btn2 ).show();
-    $( $targetDiv ).show( "slow" );
-    $( $targetDiv2 ).hide();
+  $( $btnOpenEdit ).click(function() {
+    $( $btnOpenEdit ).hide();
+    $( $btnCloseEdit ).show();
+    $( $divWithDetails ).hide();
+    $( $divWithEdits ).show( "slow" );
   });
 
-  $( $btn2 ).click(function() {
-    $( $btn ).show();
-    $( $btn2 ).hide();
-    $( $targetDiv ).hide();
-    $( $targetDiv2 ).show( "slow" );
+  $( $btnCloseEdit ).click(function() {
+    $( $btnCloseEdit ).hide();
+    $( $btnOpenEdit ).show();
+    $( $divWithEdits ).hide();
+    $( $divWithDetails ).show( "slow" );
   });
+
+
+
+
+
+
+  // const $targetDiv = document.getElementById("newDIV");
+  // const $btn = document.getElementById("show-New");
+
+  // const $targetDiv2 = document.getElementById("editDIV");
+  // const $btn2 = document.getElementById("show-Edit");
+
+  // $(function () {
+  //   $( $targetDiv ).hide();
+  //   $( $btn2 ).hide();
+  // });
+
+  // $( $btn ).click(function() {
+  //   $( $btn ).hide();
+  //   $( $btn2 ).show();
+  //   $( $targetDiv ).show( "slow" );
+  //   $( $targetDiv2 ).hide();
+  // });
+
+  // $( $btn2 ).click(function() {
+  //   $( $btn ).show();
+  //   $( $btn2 ).hide();
+  //   $( $targetDiv ).hide();
+  //   $( $targetDiv2 ).show( "slow" );
+  // });
 
 });
