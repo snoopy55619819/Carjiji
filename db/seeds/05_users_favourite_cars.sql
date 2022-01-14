a3187b754b82e5a740f7f38e38f2f306a1de1eba
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-14 03:47:24 +0000

    remove seeds for favourites table for demo purposes.

--- db/seeds/05_users_favourite_cars.sql
+++ db/seeds/05_users_favourite_cars.sql
@@ -1,12 +1,12 @@
-INSERT INTO users_favorite_cars (id, user_id, car_id)
- VALUES
- (1, 1, 1),
- (2, 1, 2),
- (3, 1, 3),
- (4, 1, 4),
- (5, 1, 5),
- (6, 2, 1),
- (7, 2, 2),
- (8, 3, 1);
+-- INSERT INTO users_favorite_cars (id, user_id, car_id)
+--  VALUES
+--  (1, 1, 1),
+--  (2, 1, 2),
+--  (3, 1, 3),
+--  (4, 1, 4),
+--  (5, 1, 5),
+--  (6, 2, 1),
+--  (7, 2, 2),
+--  (8, 3, 1);
 
 ALTER SEQUENCE users_favorite_cars_id_seq RESTART WITH 50;
