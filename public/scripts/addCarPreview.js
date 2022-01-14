d1e04b7afdef0c3071a502efe9378a03af79f6da
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-14 00:52:50 +0000

    add script to preview image when user enters a url into the urlr inpput box.

---
+++ public/scripts/addCarPreview.js
@@ -0,0 +1,18 @@
+  $(() => {
+    console.log('addCarPreview.js is ready');
+    // Preview/update image for car script
+    const $divWithImageInput = document.getElementById("addCarURLInputSection");
+    const $divWithImagePreview = document.getElementById("addCarImagePreview");
+    const $divWithCarURLValue = document.getElementById("addCarURLInputValue");
+    console.log($divWithImageInput);
+
+
+    $($divWithImageInput).change(function () {
+      console.log("changed away from url box");
+      let imageURL = $($divWithCarURLValue).val();
+      console.log(typeof imageURL);
+      $divWithImagePreview.src = imageURL;
+    });
+
+
+  })
